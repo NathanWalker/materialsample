cf46d7ea707df58440b8dca1ca1e0a0623564350
Nathan Walker <walkerrunpdx@gmail.com>
2020-08-03 22:47:02 -0700

    chore: setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "@nativescript/angular";
 
-import { AppRoutingModule } from "./app-routing.module";
+// import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { ItemsComponent } from "./item/items.component";
 import { ItemDetailComponent } from "./item/item-detail.component";
-// import { NativeScriptMaterialBottomNavigationBarModule} from "@nativescript-community/ui-material-bottomnavigationbar/angular";
+import { NativeScriptMaterialBottomNavigationBarModule} from "@nativescript-community/ui-material-bottomnavigationbar/angular";
 
 @NgModule({
     bootstrap: [
@@ -13,8 +13,8 @@ import { ItemDetailComponent } from "./item/item-detail.component";
     ],
     imports: [
         NativeScriptModule,
-        AppRoutingModule,
-        // NativeScriptMaterialBottomNavigationBarModule
+        // AppRoutingModule,
+        NativeScriptMaterialBottomNavigationBarModule
     ],
     declarations: [
         AppComponent,
